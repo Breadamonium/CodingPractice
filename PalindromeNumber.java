cd27856d5af39ded774e5b98a0b132122ddefeb3
unknown <yp89@cornell.edu>
2018-09-17 22:39:11 -0400

    no string conv

---
+++ PalindromeNumber.java
@@ -0,0 +1,18 @@
+class PalindromeNumber {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        else {
+            return x == getReverse(x);
+        }
+    }
+    
+    public int getReverse(int value) {
+        int resultNumber = 0;
+        for (int i = value; i !=0; i /= 10) {
+            resultNumber = resultNumber * 10 + i % 10;
+        }
+        return resultNumber;        
+    }
+}
