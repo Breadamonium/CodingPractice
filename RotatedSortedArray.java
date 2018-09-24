b2927baa163c4e0a6a651b83220dd3358d5ba6dc
unknown <yp89@cornell.edu>
2018-09-23 23:40:19 -0400

    logic solution

---
+++ RotatedSortedArray.java
@@ -0,0 +1,26 @@
+class RotatedSortedArray {
+    public int search(int[] nums, int target) {
+
+    }
+    
+    public int helper (int[] nums, int target, int startIndex, int endIndex) {
+    
+        if (nums[mid] > nums[start] && target < nums[start]){
+            return helper(nums,target,mid+1,end);
+        }
+        else if (nums[mid] < nums[start] && target > nums[start]) {
+            return helper(nums,target, start,mid-1);
+        }
+        else if(nums[mid] > nums[start] && target > nums[mid]} }
+            return helper(nums, target, middle, nums.length);
+        }
+        else if (nums[mid] < nums[start] && target > nums[mid]) {
+            return helper(nums, Target, 0, mid);
+        }
+        else {
+            if (nums[start] == target) return start;
+            if (nums[end] == target) return end;
+            return -1;
+        }
+    }
+}
