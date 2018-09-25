2dc58b113e59270dd1fafb19b7e4a0da7d10f6a2
unknown <yp89@cornell.edu>
2018-09-24 23:07:29 -0400

    Full solution with hashmap

---
+++ IsomorphicStrings.java
@@ -0,0 +1,24 @@
+class IsomorphicStrings {
+    public boolean isIsomorphic(String s, String t) {
+        HashMap <Character, Character> mapping = new HashMap<Character, Character>();
+        if (s.length() != t.length()) {
+            return false;
+        }
+        for(int i = 0; i<s.length(); i++) {
+            if (mapping.containsKey(s.charAt(i))) {
+                if (mapping.get(s.charAt(i))!=t.charAt(i)) {
+                    return false;
+                }
+            }
+            else {
+                if (mapping.containsValue(t.charAt(i))) {
+                    return false;
+                }
+                else {
+                    mapping.put(s.charAt(i), t.charAt(i));
+                }
+            }
+        }
+        return true;
+    }
+}
