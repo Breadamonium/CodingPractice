bf7f35296a4806ee99bb3958cab9e909473644d7
unknown <yp89@cornell.edu>
2018-11-15 23:09:14 -0500

    Ugly Number 2 #264

---
+++ UglyNumber2.java
@@ -0,0 +1,45 @@
+class UglyNumber2 {
+    
+    HashMap<Integer, Boolean> memoized = new HashMap<Integer, Boolean>();
+    
+    public int nthUglyNumber(int n) {
+        List<Integer> uglies = new ArrayList<Integer>();
+        int bounds = n;
+        for (int i = 1; i <= n; i++) {
+            if (uglies.size() == bounds) {
+                break;
+            }
+            if (isUglies(i)) {
+                memoized.put(i, true);
+                uglies.add(i);
+            }
+            else{
+                memoized.put(i, false);
+                n++;
+            }
+        }
+        return uglies.get(bounds - 1);
+    }
+    
+    public boolean isUglies(int n) {
+        
+        while(n!=1){
+            if (memoized.containsKey(n)){
+                return memoized.get(n);
+            }
+            else if (n%2 ==0) {
+                n = n/2;
+            }
+            else if (n%3 == 0) {
+                n = n/3;
+            }
+            else if (n%5 == 0) {
+                n= n/5;
+            }
+            else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
