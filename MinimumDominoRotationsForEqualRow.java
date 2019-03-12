7e7bb79a863ef7d18ea51954ab034c2541d8391a
unknown <yp89@cornell.edu>
2019-03-11 23:45:55 -0400

    leetcode2 fail

---
+++ MinimumDominoRotationsForEqualRow.java
@@ -0,0 +1,50 @@
+import java.util.HashMap;
+
+class Solution {
+    public int minDominoRotations(int[] A, int[] B) {
+        int aTracker;
+        int bTracker;
+        int prevA;
+        int prevB;
+        int answer = 0;
+        for (int i = 0; i < A.length - 1; i++) {
+            prevA = A[i];
+            prevB = B[i];
+            aTracker = A[i+1];
+            bTracker = B[i+1];
+
+            if (check(prevA, prevB, aTracker, bTracker)){
+                answer++;
+            }
+            else if (check(prevB, prevA, bTracker, aTracker){
+                answer++;
+            }
+            else{
+                return -1;
+            }
+        }
+        return answer;
+    }
+
+    public boolean check(int a, int b, int a2, int b2) {
+        if (a!=a2) {
+            if((a==b2)||(a2==b)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+HashMap<Integer, Integer> aTracker = new HashMap<>();
+HashMap<Integer, Integer> bTracker = new HashMap<>();
+
+for (int i = 0; i < A.length; i++) {
+    aTracker.putIfAbsent(A[i], 0);
+    aTracker.put(A[i], aTracker.get(A[i] + 1));
+    bTracker.putIfAbsent(B[i], 0);
+    bTracker.put(B[i], aTracker.get(B[i] + 1));
+}
+int numClosest = Collections.max(countMap.entrySet(), Comparator.comparingInt(Map.Entry::getValue)).getKey();
+
+if ()
